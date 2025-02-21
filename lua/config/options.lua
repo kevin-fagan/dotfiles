66a6c5cd85f360388fbf2e32c3a01b6a9920f694
Kevin <kevinfagan608@gmail.com>
2025-02-20 18:12:21 -0600

    updated vim opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,12 @@
 vim.opt.number = true          -- Show absolute line numbers
 vim.opt.relativenumber = true  -- Show relative line numbers
 
-vim.opt.tabstop = 4            -- Number of spaces tabs count for
-vim.opt.shiftwidth = 4         -- Size of an indentation level
-vim.opt.expandtab = true       -- Convert tabs to spaces
-vim.opt.smartindent = true     -- Smart indentation on new lines
+vim.opt.expandtab = true    -- Use spaces instead of tabs
+vim.opt.tabstop = 4         -- Number of spaces a tab counts for (e.g., 4 spaces)
+vim.opt.shiftwidth = 4      -- Number of spaces for auto-indentation
+vim.opt.softtabstop = 4     -- Number of spaces when pressing Tab
+vim.opt.smartindent = true  -- Smart indentation for new lines
+
 
 vim.opt.list = true            -- Enable showing invisible characters
 vim.opt.listchars = {
