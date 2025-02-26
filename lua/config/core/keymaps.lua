27a7e12a53bfbd42584838b0ac305b87cca91120
Kevin <kevinfagan608@gmail.com>
2025-02-25 21:59:35 -0600

    added formatter, autopairs and keymappings

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -1,2 +1,24 @@
+-- Exiting file
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+
+-- Save file
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+
+-- Exiting Insert mode
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- Switching between windows
+vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+
+-- nvim-tree
 vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-  
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
---
+++ lua/config/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+            formatters_by_ft = {
+                go = { "gofmt" },
+                lua = { "stylua" },
+            },
+        })
+    end
+}
