5c7e0656bac59ad9821673d5c62a7f07f97b92e9
Kevin Fagan <kevinfagan608@gmail.com>
2025-05-21 19:20:31 -0500

    added error diagnostic window

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -19,7 +19,7 @@ vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', opts)
 -- nvim-tree
 vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
 
--- telescope
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
@@ -27,6 +27,8 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
 vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, opts)
 vim.keymap.set('n', '<leader>fr', builtin.lsp_references, opts)
+vim.keymap.set('n', '<leader>fe', builtin.diagnostics, opts)
+vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float)
 
 -- LSP Mappings
 vim.keymap.set('n', 'ch', vim.lsp.buf.hover, opts)
--- lua/config/plugins/lsp/nvim-lspconifg.lua
+++ lua/config/plugins/lsp/nvim-lspconifg.lua
@@ -23,5 +23,20 @@ return {
         lspconfig.gopls.setup({
             capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
         })
+
+        -- Configure diagnostic window
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+            float = {
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
     end
 }
