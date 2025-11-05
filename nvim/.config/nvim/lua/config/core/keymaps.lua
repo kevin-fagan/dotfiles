12ee10d3e71bef119112ed28df131516a279e47c
Kevin <kevinfagan608@gmail.com>
2025-11-04 20:12:03 -0600

    updated keymaps

--- nvim/.config/nvim/lua/config/core/keymaps.lua
+++ nvim/.config/nvim/lua/config/core/keymaps.lua
@@ -1,30 +1,45 @@
-local ts = require('telescope.builtin')
+local ts = require("telescope.builtin")
 local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", opts)          -- Neovim: Exit file
-vim.api.nvim_set_keymap("n", "<leader>Q", ":q!<CR>", opts)         -- Neovim: Exit without saving
-vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", opts)              -- Neovim: Save file
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", opts)                  -- Neovim: Exit insert mode
-vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', opts)          -- Neovim: Jump to left window
-vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', opts)          -- Neovim: Jump to right window
-vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', opts)          -- Neovim: Jump to top window
-vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', opts)          -- Neovim: Jump to bottom window
-vim.api.nvim_set_keymap('n', '<leader>e', '$', opts)               -- Neovim: Jump to start of line
-vim.api.nvim_set_keymap('n', '<leader>s', '0', opts)               -- Neovim: Jump to end of line
-vim.api.nvim_set_keymap('n', '<leader>b', 'G', opts)               -- Neovim: Jump to top of file
-vim.api.nvim_set_keymap('n', '<leader>t', 'gg', opts)              -- Neovim: Jump to bottom of file
-vim.api.nvim_set_keymap('n', '<leader>p', '"+p', opts)             -- Neovim: Paste clipboard
+-- Navigating windows
+vim.keymap.set("n", "<leader>h", "<C-w>h", opts) -- Left window
+vim.keymap.set("n", "<leader>l", "<C-w>l", opts) -- Right window
+vim.keymap.set("n", "<leader>k", "<C-w>k", opts) -- Top window
+vim.keymap.set("n", "<leader>j", "<C-w>j", opts) -- Bottom window
 
-vim.keymap.set('n', '<leader>ff', ts.find_files, opts)             -- Telescope: File Search
-vim.keymap.set('n', '<leader>fg', ts.live_grep, opts)              -- Telescope: Text search using regex
-vim.keymap.set('n', '<leader>fb', ts.buffers, opts)                -- Telescope: Search current buffer
-vim.keymap.set('n', '<leader>fh', ts.help_tags, opts)              -- Telescope: Search help tags
-vim.keymap.set('n', '<leader>fd', ts.lsp_definitions, opts)        -- Telescope: Find definition
-vim.keymap.set('n', '<leader>fr', ts.lsp_references, opts)         -- Telescope: Find references
-vim.keymap.set('n', '<leader>fe', ts.diagnostics, opts)            -- Telescope: Diagnostics
+-- Navigating file
+vim.keymap.set("n", "<leader>e", "$", opts) -- Start of line
+vim.keymap.set("n", "<leader>s", "0", opts) -- End of line
+vim.keymap.set("n", "<leader>b", "G", opts) -- Top of file
+vim.keymap.set("n", "<leader>t", "gg", opts) -- Bottom of file
 
-vim.keymap.set('n', 'ch', vim.lsp.buf.hover, opts)                 -- Code hover
-vim.keymap.set('n', 'cr', vim.lsp.buf.rename, opts)                -- Code rename
-vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)           -- Code action
+-- Deleting content
+vim.keymap.set("n", "<leader>de", "d$", opts) -- Start of line
+vim.keymap.set("n", "<leader>ds", "d0", opts) -- End of line
+vim.keymap.set("n", "<leader>db", "dG", opts) -- Top of file
+vim.keymap.set("n", "<leader>dt", "dgg", opts) -- Bottom of file
 
-vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", opts) -- Tree: Opens File Tree
+-- Telescope
+vim.keymap.set("n", "<leader>ff", ts.find_files, opts)
+vim.keymap.set("n", "<leader>fg", ts.live_grep, opts)
+vim.keymap.set("n", "<leader>fb", ts.buffers, opts)
+vim.keymap.set("n", "<leader>fh", ts.help_tags, opts)
+vim.keymap.set("n", "<leader>fd", ts.lsp_definitions, opts)
+vim.keymap.set("n", "<leader>fr", ts.lsp_references, opts)
+vim.keymap.set("n", "<leader>fe", ts.diagnostics, opts)
+
+-- Code actions
+vim.keymap.set("n", "ch", vim.lsp.buf.hover, opts)
+vim.keymap.set("n", "cr", vim.lsp.buf.rename, opts)
+vim.keymap.set("n", "ca", vim.lsp.buf.code_action, opts)
+
+-- File tree
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
+
+-- Misc
+vim.keymap.set("n", "<leader>q", ":q<CR>", opts) -- Exit file
+vim.keymap.set("n", "<leader>Q", ":q!<CR>", opts) -- Exit without saving
+vim.keymap.set("n", "<leader>p", '"+p', opts) -- Paste clipboard
+vim.keymap.set("n", "<C-s>", ":w<CR>", opts) -- Save File
+vim.keymap.set("i", "jk", "<Esc>", opts) -- Exit insert mode
+vim.keymap.set("n", "<leader><leader>", ":b#<CR>", opts)
